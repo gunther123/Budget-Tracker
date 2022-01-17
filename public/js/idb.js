e50beb2778bceab975e7c963549ba7da60e783ff
Steven Slocum <steven.t.slocum@gmail.com>
2022-01-16 23:23:35 -0600

    added error handling for db

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,3 +5,15 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore('account_action', { autoIncrement: true });
 };
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadAccountAction();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+}
