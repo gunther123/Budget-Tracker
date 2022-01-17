eb1a50c56c54807526e7030c9192bca711e0736b
Steven Slocum <steven.t.slocum@gmail.com>
2022-01-16 23:31:17 -0600

    added save and upload function for db

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,7 +3,7 @@ const request = indexedDB.open('budget_app', 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
-    db.createObjectStore('account_action', { autoIncrement: true });
+    db.createObjectStore('new_account_action', { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
@@ -16,4 +16,43 @@ request.onsuccess = function(event) {
 
 request.onerror = function(event) {
     console.log(event.target.errorCode);
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_account_action'], 'readwrite');
+    const accountObjectStore = transaction.objectStore('new_account_action');
+    accountObjectStore.add(record);
+};
+
+function uploadAccountAction() {
+    const transaction = db.transaction(['new_account_action'], 'readwrite');
+    const accountObjectStore = transaction.objectStore('new_account_action');
+    const getAll = accountObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+
+                const transaction = db.transaction(['new_account_action'], 'readwrite');
+                const accountObjectStore = transaction.objectStore('new_account_action');
+
+                accountObjectStore.clear();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
 }
